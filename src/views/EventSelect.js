d1985e8b2b3605ce085cb4ea5ebd0bf67875985e
sharkpp <touroku@sharkpp.net>
2019-09-18 01:08:01 +0900

    変更 トップページから移動するように

--- src/views/EventSelect.js
+++ src/views/EventSelect.js
@@ -4,10 +4,12 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap'
+import { Redirect } from "react-router-dom";
 
 function EventSelectView() {
   return (
     <div>
+      <Redirect to="/7/circle/" />
       <LinkContainer to="/7/circle/">
         <Button>技術書典７</Button>
       </LinkContainer>
